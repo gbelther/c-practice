052dba5e7344c3bb74a6b2cb38e58f134f7e9e70
Gabriel Belther Santos <belthergabriel@gmail.com>
2024-04-12 20:48:34 -0300

    commit

---
+++ chapter2/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    int age = 29;
+    age = 30;
+
+    char initial = 'D';
+    float temperature = 68.5; // menos preciso
+    double number = 3.14159; // mais preciso
+}
