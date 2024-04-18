4963df271f2c406e4c46946e8d13ebd22bce44fc
Gabriel Belther Santos <belthergabriel@gmail.com>
2024-04-18 12:31:43 -0300

    chapter2

--- chapter2/main.c
+++ chapter2/main.c
@@ -2,9 +2,19 @@
 
 int main(void) {
     int age = 29;
-    age = 30;
+    age = 29;
 
     char initial = 'D';
     float temperature = 68.5; // menos preciso
-    double number = 3.14159; // mais preciso
+    double numberPi = 3.14159; // mais preciso
+
+    printf("My age is %i\n", age);
+
+    // int number;
+    // printf("Please enter your age:");
+    // scanf("%i", &number);
+    // printf("Your age is %i\n", number);
+
+    const int LUCKY_NUM = 7;
+    printf("My lucky number is: %i\n", LUCKY_NUM);
 }
