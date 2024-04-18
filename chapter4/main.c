f10877cd376ed7e391023ed0961d4214acd2a141
Gabriel Belther Santos <belthergabriel@gmail.com>
2024-04-18 12:57:36 -0300

    chapter4

---
+++ chapter4/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // int age;
+
+    // printf("Please enter your age: ");
+
+    // scanf("%i", &age);
+
+    // if (age < 18)
+    // {
+    //     printf("You need to be over 18 years old to continue\n");
+    // }
+    // else if (age < 21)
+    // {
+    //     printf("You need to be over 21\n");
+    // }
+    // else
+    // {
+    //     printf("You are over 18 and older than 21 so you can continue \n");
+    // }
+
+    int x = 10;
+    
+    int y = (x > 5) ? 100 : 200;
+    
+    printf("x: %i\n", x); // x: 10
+    
+    printf("y: %i\n", y);  // y: 100
+}
