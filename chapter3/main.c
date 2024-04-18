bf9a6475d1ca073bda0b1314a9ff0b39b2e63ec1
Gabriel Belther Santos <belthergabriel@gmail.com>
2024-04-18 12:48:33 -0300

    chapter3

---
+++ chapter3/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main(void) {
+    // int a = 10;
+    // int b = 2;
+
+    // int sum = a + b;
+    // printf("Sum: %i\n", sum);
+
+    // int difference = a - b;
+    // printf("Difference: %i\n", difference);
+
+    // int product = a * b;
+    // printf("product: %i\n", product);
+
+    // int quotient = a / b;
+    // printf("quotient: %i\n", quotient);
+
+    // int remainder = a % b;
+    // printf("remainder: %i\n", remainder);
+
+    // int a = 5;
+    // int b = 5;
+
+    // int result = (a == b);
+    // printf("Result: %i\n", result);
+
+    // int a = 5;
+    // int b = 3;
+
+    // int result = (a != b);
+    // printf("Result: %i\n", result);
+
+    // (>=, <=, >, <)
+    // (&&, ||, !)
+    // (+=, -=, *=, /=, %=)
+    // (++, --)
+}
